a28481e13baf9a65664cef8e30536891cc6c57aa
Paul Blair <ciriwe@phobot.net>
2014-06-12 19:01:06 -0400

    Some setup for the Denbow parser.

--- App/Script.fsx
+++ App/Script.fsx
@@ -7,14 +7,33 @@
 
 open EmailParser.Utils.Mime
 open EmailParser.Utils.Text
-open ODonnellParser.Parser
-open ODonnellParser.Utils
 open EmailParser.Utils.Collections
 open System.Text.RegularExpressions
 
 System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
-// let message = loadMimeMessageFrom("../ODonnellParser/Email.odonnell")
-let message = loadMimeMessageFrom("../ODonnellParser/Email.denbow")
+
+let message = loadMimeMessageFrom("../ODonnellParser/Email.odonnell")
+let parse = ODonnellParser.Parser.parse
+let extractDateStringFrom = ODonnellParser.Parser.extractDateStringFrom
+let extractTimeStringFrom = ODonnellParser.Parser.extractTimeStringFrom
+let extractRemainingDataForCurrentCalendarEntry = ODonnellParser.Parser.extractRemainingDataForCurrentCalendarEntry
+let calendarEntryFrom = ODonnellParser.Parser.calendarEntryFrom
+let parseMail = ODonnellParser.Parser.parseMail
+let PreIntro = ODonnellParser.Parser.PreIntro
+let IntroPart = ODonnellParser.Parser.IntroPart
+
+(*
+
+let message = loadMimeMessageFrom("../DenbowParser/Email.denbow")
+let parse = DenbowParser.Parser.parse
+let extractDateStringFrom = DenbowParser.Parser.extractDateStringFrom
+let extractTimeStringFrom = DenbowParser.Parser.extractTimeStringFrom
+let extractRemainingDataForCurrentCalendarEntry = DenbowParser.Parser.extractRemainingDataForCurrentCalendarEntry
+let calendarEntryFrom = DenbowParser.Parser.calendarEntryFrom
+let parseMail = DenbowParser.Parser.parseMail
+let PreIntro = DenbowParser.Parser.PreIntro
+let IntroPart = DenbowParser.Parser.IntroPart
+*)
 
 let messageParts = parse message.MessageLines PreIntro
 let nonIntroParts = messageParts |> List.filter (function |IntroPart(_) -> false | _ -> true)
--- DenbowParser/Parser.fs
+++ DenbowParser/Parser.fs
@@ -3,28 +3,26 @@
 open EmailParser.Types
 open EmailParser.Utils.Collections
 
-(*
+
 type State =
     | PreIntro
     | Intro
-    | Date
-    | EventHeader
-    | EventLocation
-    | EventDescription
+    | EventDate
     | RSVPLink
+    | EventHeader
+    | EventDateTime
+    | EventDescription
+    | EventTrailer
+    | EmailFooter
 
 type MessagePart =
     | IntroPart of list<string>
-    | DatePart of string
-    | TimePart of string
-    | TitlePart of string
-    | LocationPart of list<string>
-    | DescriptionPart of list<string>
     | RsvpLinkPart of string
-    *)
+    | TitlePart of string
+    | DateTimePart of string
+    | DescriptionPart of list<string>
 
 
-
-// let parseMail (message: EmailMessage) : EmailData = 
+//let parseMail (message: EmailMessage) : EmailData = 
 //    let messageParts = parse message.MessageLines PreIntro
 //    parseIntoEmailData message.Sender message.SentDate messageParts
